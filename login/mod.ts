ed3744b91bd8dd863e23c162711dabc582244319
Daniel Lamando <dan@danopia.net>
2025-04-17 10:43:50 +0200

    Start monorepo for the smaller and/or default apps

---
+++ login/mod.ts
@@ -0,0 +1,166 @@
+#!/usr/bin/env -S deno run --unstable-kv --unstable-broadcast-channel --unstable-cron --unstable-http --watch --allow-env --allow-sys --allow-read --allow-write=${HOME}/.local/share/dist-app --allow-net --no-prompt
+import "https://deno.land/x/observability@v0.6.1/preconfigured/from-environment.ts";
+
+import { DenoKvStorage } from "https://uber.danopia.net/dist-app-deno/52605ca9fa7d2f5d0a26961f29f1d864c9966352/server-sdk/modules/storage-deno-kv/mod.ts";
+
+// another example usage @ https://dash.deno.com/projects/weak-squirrel-54
+import { OidcIssuer } from "https://uber.danopia.net/dist-app-deno/52605ca9fa7d2f5d0a26961f29f1d864c9966352/lib/oidc/kv-issuer.ts";
+import { LoginServerApi } from "https://uber.danopia.net/dist-app-deno/52605ca9fa7d2f5d0a26961f29f1d864c9966352/apis/login-server/definitions.ts";
+import { setupSingleSite } from "https://uber.danopia.net/dist-app-deno/52605ca9fa7d2f5d0a26961f29f1d864c9966352/server-sdk/core/single-site.ts";
+import { CookieAuthnMethod } from "https://uber.danopia.net/dist-app-deno/52605ca9fa7d2f5d0a26961f29f1d864c9966352/lib/auth/authn-methods/cookie.ts";
+import { PasskeyAuthnMethod } from "./lib/passkey.ts";
+
+const server = await setupSingleSite(async (app, siteBaseUrl) => {
+  app.auth.addAuthnMethod(new CookieAuthnMethod({
+    sessionLengthDays: 7,
+  }));
+  app.auth.addAuthnMethod(new PasskeyAuthnMethod('dist.app identity', new URL(siteBaseUrl).hostname));
+
+  const kv = await DenoKvStorage.existingKv!;
+  const oidcIssuer = new OidcIssuer(kv, ['oidc', 'ecdsa'], {
+    name: 'ECDSA',
+    hash: 'SHA-384',
+    namedCurve: 'P-384',
+    // for RSA:
+    // name: "RSASSA-PKCS1-v1_5",
+    // hash: "SHA-512",
+    // modulusLength: 2048,
+    // publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+  });
+  oidcIssuer.dropOldKeys();
+
+  app.mountPathHandler('/.well-known/openid-configuration', [], () => {
+    return Response.json({
+      // https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderMetadata
+      "issuer": `${siteBaseUrl}`,
+      "authorization_endpoint": `${siteBaseUrl}/oidc/authorize`,
+      "token_endpoint": `${siteBaseUrl}/oidc/redeem`,
+      // userinfo_endpoint
+      "jwks_uri": `${siteBaseUrl}/.well-known/jwks.json`,
+      // registration_endpoint
+      "scopes_supported": [ "openid", "profile", "email" ],
+      // TODO: update these based on what we actually did
+      "response_types_supported": [ "id_token" ], // not to spec
+      "response_modes_supported": [ "query" ],
+      "grant_types_supported": [ "authorization_code" ], // not to spec
+      "subject_types_supported": [ "public" ],
+      "id_token_signing_alg_values_supported": [ "ES384", /*"RS512"*/ ], // not to spec
+      "token_endpoint_auth_methods_supported": [ "private_key_jwt" ],
+      "token_endpoint_auth_signing_alg_values_supported": [ "ES384" ],
+    });
+  });
+
+  app.mountPathHandler('/.well-known/jwks.json', [], async () => {
+    return Response.json({
+      "keys": await oidcIssuer.getCurrentKeys(),
+    });
+  });
+
+  const loginServerApi = new LoginServerApi(app.auth.index);
+
+  app.mountPathHandler('/oidc/authorize', ['authed'], async (req, _match, user) => {
+    const reqUrl = new URL(req.url);
+    if (!user) throw new Error('BUG: Missing auth contexxt');
+
+    const response_type = reqUrl.searchParams.get('response_type'); // 'code'
+    const client_id = reqUrl.searchParams.get('client_id'); // this.props.rootUrl
+    const redirect_uri = reqUrl.searchParams.get('redirect_uri'); // origUrl.toString()
+    const scope = reqUrl.searchParams.get('scope'); // 'openid'
+    const state = reqUrl.searchParams.get('state'); // 'TODO:STATE'
+
+    if (response_type !== 'code') throw new Error(`unexpected response_type`);
+    if (!client_id?.startsWith('https://')) throw new Error(`this server wants URLs as client_id`);
+    if (!redirect_uri) throw new Error(`expected redirect_uri`);
+    if (!redirect_uri.startsWith(client_id)) throw new Error(`redirect_uri should be within client-id`);
+    if (scope !== 'openid') throw new Error(`unexpected scope`);
+    if (typeof state !== 'string') throw new Error(`this server requires state values`);
+
+    // TODO(SECURITY): further validate redirect_uri
+
+    const expiresAfter = new Date();
+    expiresAfter.setSeconds(expiresAfter.getSeconds() + 90);
+
+    const code = crypto.randomUUID().split('-')[4];
+    await loginServerApi.createOpenidConnectCode({
+      metadata: {
+        name: code,
+      },
+      spec: {
+        state,
+        callbackUrl: redirect_uri,
+        issuer: siteBaseUrl,
+        audience: client_id,
+        expiresAfter,
+        userName: user.metadata.name,
+        claimsJson: JSON.stringify({
+          'profile': {
+            'username': user.metadata.name,
+            // 'domain': user.spec.
+            'display_name': user.spec.profile.displayName,
+          },
+          'iss': siteBaseUrl,
+          'aud': client_id,
+          'sub': `user:uid:${user.metadata.uid}`,
+          'scope': scope,
+          'amr': ['swk'], // should be hwk when passkey is not synced, pwd for password, otp for magic link
+        }),
+      },
+    });
+
+    const newUrl = new URL(redirect_uri);
+    newUrl.searchParams.set('code', code);
+    newUrl.searchParams.set('state', state);
+
+    return Promise.resolve(new Response('Redirecting to application', {
+      status: 302,
+      headers: {
+        location: newUrl.toString(),
+      },
+    }));
+  });
+
+  app.mountPathHandler('/oidc/redeem', [], async req => {
+    if (req.method !== 'POST') return new Response('405', { status: 405 });
+    if (req.headers.get('content-type') !== 'application/x-www-form-urlencoded') {
+      return new Response('want application/x-www-form-urlencoded', { status: 400 });
+    }
+    const reqBody = new URLSearchParams(await req.text());
+
+    const code = reqBody.get('code'); // 'TODO:CODE'
+    const grant_type = reqBody.get('grant_type'); // 'authorization_code'
+    const redirect_uri = reqBody.get('redirect_uri');
+
+    if (grant_type !== 'authorization_code') throw new Error(`unexpected grant_type`);
+    // if (code !== 'TODO:CODE') throw new Error(`unexpected code`);
+    if (typeof code !== 'string') throw new Error(`expected code`);
+
+    const codeData = await loginServerApi.getOpenidConnectCode(code);
+    if (!codeData) throw new Error(`unknown code`);
+    if (codeData.spec.callbackUrl !== redirect_uri) throw new Error(`unexpected redirect_uri`);
+
+    const accessJwt = await oidcIssuer.signJwt({
+      ...JSON.parse(codeData.spec.claimsJson),
+    });
+
+    const idJwt = await oidcIssuer.signJwt({
+      ...JSON.parse(codeData.spec.claimsJson),
+      // Claims are added based on scopes, see https://openid.net/specs/openid-connect-core-1_0.html#Claims
+      preferred_username: codeData.spec.userName,
+    });
+
+    // TODO: keep instead, set status and allow for automatic cleanup
+    await loginServerApi.deleteOpenidConnectCode(code);
+
+    return Response.json({
+      "access_token": accessJwt,
+      "token_type": "Bearer",
+      "expires_in": 3600,
+      // "refresh_token": "tGzv3JOkF0XG5Qx2TlKWIA",
+      "id_token": idJwt,
+    });
+  });
+
+}, {
+  authKvPrefix: ['entities', 'local-index'],
+});
+server.serveHttp();
---
+++ tinytools/mod.tsx
@@ -0,0 +1,167 @@
+#!/usr/bin/env -S deno run --unstable --watch --allow-env --allow-sys --allow-read --allow-net=0.0.0.0,dist-v1alpha1.deno.dev,otel.devmode.cloud
+/** @jsx h */
+/** @jsxFrag Fragment */
+import "https://deno.land/x/observability@v0.6.1/preconfigured/from-environment.ts";
+import html, { h } from "https://deno.land/x/htm@0.2.1/mod.ts";
+
+import { JsonUrlLoader } from "https://uber.danopia.net/dist-app-deno/52605ca9fa7d2f5d0a26961f29f1d864c9966352/server-sdk/modules/loader-json-url.ts";
+import { svgForIcon } from "https://uber.danopia.net/dist-app-deno/52605ca9fa7d2f5d0a26961f29f1d864c9966352/apis/manifest/icon.ts";
+import { setupSingleSite } from "https://uber.danopia.net/dist-app-deno/52605ca9fa7d2f5d0a26961f29f1d864c9966352/server-sdk/core/single-site.ts";
+
+const server = await setupSingleSite((app, _siteBaseUrl) => {
+
+  app.mountWebManifest('/app.webmanifest', {
+    name: 'Tiny Web-based Tools',
+    short_name: 'Tiny Tools',
+    start_url: '.',
+    id: '/',
+    display: 'minimal-ui',
+    theme_color: 'rgb(51, 102, 51)',
+    background_color: '#333',
+    description: 'Various simple developer utilities for working with encodings and more',
+    icons: [{
+      sizes: 'any',
+      type: 'image/svg+xml',
+      purpose: 'maskable',
+      src: 'app-icon-maskable.svg',
+    }, {
+      sizes: 'any',
+      type: 'image/svg+xml',
+      purpose: 'any',
+      src: 'app-icon-full.svg',
+    }],
+  });
+  app.mountPathHandler('/app-icon-maskable.svg', [], () => new Response(svgForIcon({
+    type: 'glyph',
+    glyph: {
+      text: '👨‍💻',
+      backgroundColor: 'rgb(51, 102, 51)',
+    },
+  }, '5 5 50 50'), {
+    headers: {
+      'content-type': 'image/svg+xml',
+    },
+  }));
+  app.mountPathHandler('/app-icon-full.svg', [], () => new Response(svgForIcon({
+    type: 'glyph',
+    glyph: {
+      text: '👨‍💻',
+      backgroundColor: 'rgb(51, 102, 51)',
+    },
+  }, '10 10 40 40'), {
+    headers: {
+      'content-type': 'image/svg+xml',
+    },
+  }));
+
+  app.mountPublicApp('/tools/http-client/', {
+    loader: new JsonUrlLoader('https://dist-v1alpha1.deno.dev/debug/dump-catalog/86834d4d'),
+    activityName: 'main',
+    persistKey: 'http-client',
+    requireRoles: [],
+  });
+
+  app.mountPublicApp('/tools/', {
+    loader: new JsonUrlLoader('https://dist-v1alpha1.deno.dev/debug/dump-catalog/5c0c3d8f'),
+    activityName: 'launcher',
+    persistKey: 'toolbelt',
+    requireRoles: [],
+  });
+
+  app.mountPathHandler('/', [], _ => {
+    return html({
+      lang: "en",
+      title: "Tiny Web Tools",
+      meta: {
+        description: "Various simple developer utilities for working with encodings and more",
+      },
+      links: [
+        { rel: 'manifest', href: '/app.webmanifest' },
+      ],
+      styles: [
+        `h1, h2 {
+          margin: 0.3em 1em;
+          padding: 1em 0 0.5em;
+          color: #999;
+        }
+        body {
+          margin: 0;
+          background-color: #222;
+          color: #fff;
+          font-family: monospace;
+        }
+        #tool-list {
+          list-style: none;
+          padding: 0;
+          margin: 1em;
+          box-sizing: border-box;
+          display: flex;
+          flex-wrap: wrap;
+          gap: 1.5em;
+        }
+        @media (min-width: 640px) {
+          body {
+            font-size: 1.1em;
+          }
+        }
+        @media (min-width: 1200px) {
+          body {
+            font-size: 1.2em;
+          }
+        }
+        .wrap {
+          max-width: 100em;
+          padding: 0.5em;
+          background-color: #333;
+        }
+        #tool-list li {
+          flex: 20em 1 0;
+          display: grid;
+        }
+        #tool-list a {
+          display: flex;
+          text-align: center;
+          align-items: center;
+          justify-content: center;
+          padding: 1em;
+          box-sizing: border-box;
+          font-size: 1.6em;
+          background-color: rgba(200, 200, 200, 0.5);
+          color: #fff;
+          transition: background-color 0.2s linear;
+        }
+        #tool-list a:not(:hover) {
+          text-decoration: none;
+          background-color: rgba(200, 200, 200, 0.3);
+        }`.replace(/^ {6}/gm, ''),
+      ],
+      body: (
+        <div class="wrap">
+          <h1>👨&zwj;💻 Tiny Tools</h1>
+          <h2>Encodings and Conversions</h2>
+          <ul id="tool-list">
+            <li><a href="/tools/base64">Base64 encode/decode</a></li>
+            <li><a href="/tools/pretty-json">JSON formatter</a></li>
+            <li><a href="/tools/jwt">JWT inspector</a></li>
+            <li><a href="/tools/urlencode">urlencode & urldecode</a></li>
+            <li><a href="/tools/timestamp">UNIX epoch timestamps</a></li>
+            <li />
+            <li />
+            <li />
+          </ul>
+          <h2>Lookups</h2>
+          <ul id="tool-list">
+            <li><a href="/tools/aws-ips">IP ownership</a></li>
+            <li><a href="/tools/google-dns">DNS resolver</a></li>
+            <li><a href="/tools/http-client/">HTTP client</a></li>
+            <li />
+            <li />
+            <li />
+          </ul>
+        </div>
+      ),
+    });
+  });
+
+});
+server.serveHttp();
